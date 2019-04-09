f5e2345e1462793da105a46e8a78845f8883748e
Yogen Rai <yogen.rai.992@gmail.com>
2019-04-09 08:07:45 -0700

    configuring fe build

--- scripts/fe_build.sh
+++ scripts/fe_build.sh
@@ -23,12 +23,45 @@ case "${environment}" in
         ;;
 esac
 
-if [[ "${source_branch}" = 'develop' ]]; then
-    $(dirname $0)/repo_branch.sh -b "${source_branch}" -r q-collect-ui -u $user -v "${version}"
-else
-    $(dirname $0)/repo_branch.sh -b "${source_branch}" -r q-collect-ui -u $user -v "${version}"
-fi
+$(dirname $0)/repo_branch.sh -b "${source_branch}" -r q-collect-ui -u $user -v "${version}"
 
 echo "Building and deploying ${version} to ${environment}:"
+
+body="{
+  \"request\": {
+    \"message\": \"Build from ${branch} triggered by ${user}\",
+    \"branch\": \"${branch}\",
+    \"config\": {
+      \"merge_mode\":[\"replace\"],
+      \"env\": {
+        \"global\": [
+          \"deploy_env=${environment}\"
+        ]
+      },
+      \"before_script\": \"chmod +x ./travis/scripts/deploy.sh\",
+      \"script\": \"./travis/scripts/deploy.sh\",
+      \"deploy\": {
+        \"provider\": \"s3\",
+        \"access_key_id\": \"${aws_access_key_id}\",
+        \"secret_access_key\":\"${aws_secret_access_key}\",
+        \"bucket\":\"q-collect\",
+        \"skip_cleanup\" : true,
+        \"local_dir\": \"build\",
+        \"upload_dir\": \"q-collect-web-${environment}\"
+      }
+    }
+  }
+}"
+
+#TRAVIS_API_TOKEN="${TRAVIS_API_TOKEN}"
+
+curl --silent --output /dev/null -X POST \
+ -H "Content-Type: application/json" \
+ -H "Accept: application/json" \
+ -H "Travis-API-Version: 3" \
+ -H "Authorization: token ${TRAVIS_API_TOKEN}" \
+ -d "$body" \
+ https://api.travis-ci.com/repo/miyozinc%2Fq-collect-ui/requests
+
 
 exit 0
